fb0c141efb9d4b5e7d24b1a206f8991714e61ff1
Alvin Baldemeca <baldea98@uw.edu>
2018-10-14 17:01:07 -0700

    added a way to write files and comments on code

--- afile_client.erl
+++ afile_client.erl
@@ -1,15 +1,26 @@
 -module(afile_client).
--export([ls/1, get_file/2]).
+-export([ls/1, get_file/2,create_write_file/3]).
+
+
 ls(Server) ->
+    %Send a message to the server { send the PID and the list_dir atom}
     Server ! {self(), list_dir},
-    receive
-        {Server, FileList} ->
+    receive                         %recieve the replay from the server
+        {Server, FileList} ->       %we get the server PID and the FileList  
         FileList
     end.
 
 get_file(Server, File) ->
-    Server ! {self(), {get_file, File}},
-    receive
-        {Server, Content} ->
-            Content
+    Server ! {self(), {get_file, File}},    %Send a message to the Server (PID) get_file and Filename
+    receive                                 %Listen for a replay before ending
+        {Server, Content} ->                %Pattern match on the PID and Content returned from the server
+            Content                         %Return the content
     end.
+
+create_write_file(Server, Filename, Content) ->  %create a function to send a message to the server to write a file with contents
+    Server ! {self(),{put_file,Filename,Content}},  %Send a message to the server
+    receive
+        {Server,Return} ->                          %Recieve from Server with the return from the write operation
+            Return
+    end.
+
--- afile_server.erl
+++ afile_server.erl
@@ -1,12 +1,16 @@
 -module(afile_server).
 -export([start/1, loop/1]).
-start(Dir) -> spawn(afile_server, loop, [Dir]).
-loop(Dir) ->
+start(Dir) -> spawn(afile_server, loop, [Dir]).     %Create a new process for the file server
+loop(Dir) ->                                        %Loop for ever listening to any messages
     receive
-        {Client, list_dir} ->
-            Client ! {self(), file:list_dir(Dir)};
-        {Client, {get_file, File}} ->
-            Full = filename:join(Dir, File),
-            Client ! {self(), file:read_file(Full)}
+        {Client, list_dir} ->                       %If we get a message saying list_dir
+            Client ! {self(), file:list_dir(Dir)};  %Replay to the Client using the BIF file:list_dir of the initial directory
+        {Client, {get_file, File}} ->               %If we get a message saying get_file 
+            Full = filename:join(Dir, File),        % get the fullpath for the file then 
+            Client ! {self(), file:read_file(Full)}; %Send a message back to the client of the content of the file
+        %Add a message to put_file
+        {Client, {put_file,Filename,Content}} ->                %pattern match PID and message tuple with message id put_file
+            Full = filename:join(Dir,Filename),                 %Write the file to the current directory
+            Client ! {self(), file:write_file(Full,Content)}    %Finally return a message to the client
     end,
     loop(Dir).
