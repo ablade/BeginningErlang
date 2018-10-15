3f8a5e5cd156912cbfd468f27a8c154a029a7bda
Alvin Baldemeca <baldea98@uw.edu>
2018-10-15 09:32:10 -0700

    Finished up math_fucntions.erl

--- math_functions.erl
+++ math_functions.erl
@@ -12,7 +12,7 @@
 
 -module(math_functions).
 
--export([even/1,odd/1,filter/2]).
+-export([even/1,odd/1,filter/2,odds_and_evens/1]).
 %-export([even/1,odd/1,filter/2,split/1]).
 
 %Item 5
@@ -23,3 +23,13 @@ odd(X) when is_integer(X) -> X rem 2 /= 0.
 filter(F,L) -> [X || X <- L, F(X)].
 
 %Item 7  
+odds_and_evens(L) when is_list(L) ->
+    odds_and_evens_acc(L,[],[]).
+
+odds_and_evens_acc([H|T],Odds,Evens) ->
+    case( H rem 2) of
+        1 -> odds_and_evens_acc(T,[H|Odds],Evens);
+        0 -> odds_and_evens_acc(T,Odds,[H|Evens])
+    end;
+odds_and_evens_acc([],Odds,Evens) ->
+    {lists:reverse(Odds),lists:reverse(Evens)}.
